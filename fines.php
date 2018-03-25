2b6bf4361069d2827d3cb083483bc4cbbdc8f0ff
Palaumi <37519758+Palaumi@users.noreply.github.com>
2018-03-25 14:57:43 -0500

    Library management system

---
+++ fines.php
@@ -0,0 +1,62 @@
+<?php
+	$con = new mysqli("localhost","root","root","library",3307);
+	$card_id=$_GET["card_id"];
+	$x = intval($card_id);
+	
+	$sql5 = "select loan_id, fine_amt from book_loans natural join fines where date_in <= due_date";
+	$result3 = $con->query($sql5);
+		while($row1 = $result3->fetch_assoc())
+		{
+			$l1=$row1['loan_id'];
+			$f1=$row1['fine_amt'];
+			$sql6 = "update fines natural join book_loans set fine_amt=0 where date_in <= due_date";
+			$result4 = $con->query($sql6);
+		}
+	
+
+	$sql = "select * from book_loans natural join fines where card_id=$x";
+	$result = $con->query($sql);
+
+	if ($result->num_rows > 0) 
+	{
+		echo "<table border='2' style='width:100%'><tr><th>Loan_ID</th><th>ISBN</th><th>Card_ID</th><th>Fine_Amount</th><th>Paid</th><th></th></tr>";
+		while($row = $result->fetch_assoc()) 
+		{
+			$c=$row['card_id'];
+			$f=$row['fine_amt'];
+			$l=$row['loan_id'];
+			$sql2 = "select date_in from book_loans where loan_id=$l";
+			$result2 = $con->query($sql2);
+			$row2 = $result2->fetch_assoc();
+			$i = $row2['date_in'];
+			echo "<tr>";
+			echo "<td>" . $row['loan_id'] . "</td>";
+			echo "<td>" . $row['isbn'] . "</td>";
+			echo "<td>" . $row['card_id'] . "</td>";
+			echo "<td>" . $row['fine_amt'] . "</td>";
+			if($row['paid']==0 && $i!=null)
+				echo "<td><button type='button' onclick=pay($c,$f,$l);>Pay</td>";
+			else if($i==null)
+				echo "<td><button type='button' disabled>Pay</td>";
+			else
+				echo "<td>Paid</td>";
+			echo "</tr>";
+		}	
+		echo "</table>";
+		$sql1="select sum(fine_amt) from fines natural join book_loans where card_id=$x and paid=0 group by card_id";
+		$result1 = $con->query($sql1);
+		$row = $result1->fetch_assoc();
+		$i = $row['sum(fine_amt)'];
+		if($i>0)
+			echo "Total outstanding balance = $i $.";
+		else
+			echo "No outstanding balance";
+		echo "<br><br><button type='button' onclick=fun();>Go Back to main page";
+	}	
+	else
+	{
+		echo "No fine amounts to show";
+		echo "<br><br><button type='button' onclick=fun();>Go Back to main page";
+	}
+	$con->close();
+?>
---
+++ readme.txt
@@ -0,0 +1,7 @@
+-----------------------USER GUIDE TO USE THE LIBRARY MANAGEMENT SYSTEM-----------------------------------------------------------------
+
+-----1)Install Mamp and start your Mamp server
+-----2)Change the port of MySQL to 3307 from Preferences.
+-----2)Import the data.sql file for the database in phpmyadmin.	
+-----3)Unzip the file pnp170130_cs6360.zip in the htdocs folder of mamp and type this in your url "localhost/pnp170130_cs6360/library.html".
+-----4)Use the UI which is displayed when you go to the link "localhost/pnp170130_cs63630/library.html" to navigate through various modules like searching the books,add borrowers,return book and check fine/pay fines.
